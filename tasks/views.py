5771fa9acb42deaf68d244d650cbec27a4bb598b
Yahor Berdnikau <yahor.berdnikau@instinctools.com>
2023-06-05 22:39:41 +0300

    removed debug logger

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,9 @@
 from datetime import datetime
 
 from django.shortcuts import render
-from structlog import get_logger
 from tasks.models import Task
-
-logger = get_logger()
 
 
 def get_task(request, difficulty):
     task = Task.objects.get(week_number=datetime.today().isocalendar()[1], difficulty=difficulty)
-    logger.warn(task.as_dict())
     return render(request, "task.html", context=task.as_dict())
